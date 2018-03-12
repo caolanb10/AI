b11247f9f045dd61b7124bec80bf64378bc0cdbc
Caolan Barry <barryc10@lg12l13.scss.tcd.ie>
2018-03-12 13:24:20 +0000

    continued

--- ai.py
+++ ai.py
@@ -11,6 +11,7 @@ returns the values
 qn(s,exercise) and qn(s,relax)
 
 '''
+
 def startval(S, Action):
     index = 0
     if(S=="fit"):
@@ -22,27 +23,41 @@ def startval(S, Action):
     else:
         return Relax[index][0]*Relax[index][1] + Relax[index+1][0]*Relax[index+1][1]
 
-def qval(S, matrix, N):
+def qval(S, A, N):
     if(N==0):
-        return startval(S,N)
+        return startval(S,A)
     else:
-        return
+        return startval(S,A) + y*( p(S,A,"fit")*V("fit",N-1)+p(S,A,"unfit")*V("unfit",N-1) ) 
+
+def V(S,N):
+    exercise = qval(S,"exercise",N)
+    relax = qval(S,"relax",N)
+    return max(exercise, relax)
 
 def p(S,A,State):
     index = 0
+    index2 = 0
+    if(State=="unfit"):
+	index2 = 1
     if(S=="unfit"):
         index = 2
     if(A=="exercie"):
-        return Exercise[index][0]
-
+        return Exercise[index+index2][0]
+    else:
+	return Relax[index+index2][0]
 
 # variable values
 n = 10
 y = 0.5
 s = 0 # s = {fit, unfit}, 0=fit 1=unfit
+e = "exercise"
+r = "relax"
+
 
 assert(((y>0) and (y<1)) and ((s==0) or (s==1)))
 
 Exercise = [[0.99, 8], [0.01, 8], [0.2, 0], [0.8, 0]]
 Relax = [[0.7, 10], [3, 10], [0, 5], [1, 5]]
-FitExercise = qval
+
+exerciseVal = qval(s,e,n)
+relaxVal = qval(s,r,n)
